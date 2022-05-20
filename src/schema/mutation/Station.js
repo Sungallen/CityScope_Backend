676905f21ee70d5188ad18b435a104403b37b663
Sungallen <a0975671396@gmail.com>
2022-05-20 22:26:59 +0800

    second

--- src/schema/mutation/Station.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CREATE_USER = void 0;
-const Stations_1 = require("../TypeDefs/Stations");
-exports.CREATE_USER = {
-    type: Stations_1.StationType
-};
